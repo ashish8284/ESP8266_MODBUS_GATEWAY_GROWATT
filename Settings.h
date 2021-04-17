d8f9229029bacd1d4c15be51a4fb903c8a3541b1
Techno Automaton <ashishamu146@gmail.com>
2021-04-17 21:16:21 +0530

    Update Settings.h

--- Settings.h
+++ Settings.h
@@ -4,33 +4,33 @@ int Tx = 12;
 int DE_RE = 4; // For MAX485 chip
 
 //Wifi router Credentials
-const char* ssid             = "Ashish Patel02";
-const char* passuint16_t     = "Aradhya$146";
+const char* ssid             = ""; //Enter Wifi router SSID
+const char* passuint16_t     = ""; //Enter WIFI router password
 
 //MQTT Server Setting
-bool MQTT_Enable = 1;
-int Re_Connect_Interval = 60000;
-const char* mqtt_server      = "192.168.1.100";
-const char* mqttUser         = "openhabian";
-const char* mqttPassuint16_t = "openhabian";
-const char* mqttClientid     = "ABCD";
-unsigned long  Window_Time = 5000;
+bool MQTT_Enable = 1; //Set 0 if MQTT function not required
+int Re_Connect_Interval = 60000; //Retrial interval in case MQTT server connection is not connected
+const char* mqtt_server      = ""; //Set ip address of MQTT sserver i.e. :-  "192.168.1.1"
+const char* mqttUser         = ""; //MQTT server user name. Keep blank if no user name is assigned to MQTT server.
+const char* mqttPassuint16_t = ""; //MQTT server password. keep blank if password is not set in MQTT server.
+const char* mqttClientid     = "ABCD"; //Unique Client id. In case multiple device in same network then this required to be different for each ESP.
+unsigned long  Window_Time = 5000; //Data posting Time interval for MQTT. 
 
 //Settings for AP Mode
-const char* AP_ssid             = "Techno_Growatt";
-const char* AP_passuint16_t     = "12345678";
-IPAddress AP_IP(192, 168, 11, 4);
-IPAddress AP_gateway(192, 168, 11, 1);
-IPAddress AP_subnet(255, 255, 255, 0);
+const char* AP_ssid             = "Techno_Growatt"; //SSID for AP mode. ESP create WIFI network wiht SSID
+const char* AP_passuint16_t     = "12345678"; //Password for AP mode. User can connect with ESP using this password.
+IPAddress AP_IP(192, 168, 11, 4); //IP address of ESP for AP mode operation. We can use this IP to access webpage. 
+IPAddress AP_gateway(192, 168, 11, 1); //Gateway of ESP for AP mode.
+IPAddress AP_subnet(255, 255, 255, 0); //Subnet of ESP for AP mode.
 
 //Adaftruit.io credential
-#define AIO_USERNAME    "Ashish_82848284"
-#define AIO_KEY         "82184014511d457ca7c0775480419bb2"
+#define AIO_USERNAME    "" //User name of Adafruit.io account.
+#define AIO_KEY         "" //AIO Key of Adafruit.io account
 
 //Adafruit Setting
-bool Ada_MQTT_Enable = 1;
-unsigned long AdaMqttDly_Window = (1000 *60)* 1;
+bool Ada_MQTT_Enable = 1; //Set this to enable data posting on Adafruit.io
+unsigned long AdaMqttDly_Window = (1000 *60)* 1; //Data posting Time interval for Adafruit server data posting. 
 
 //Modbus Slave address
-byte Start_ID = 1;
-byte End_Id = 1;
+byte Start_ID = 1; //Modbus Slave Address of Inverter.
+byte End_Id = 1; //Number of inverters in single loop(currently multiple inverter data posting is not implemented).
